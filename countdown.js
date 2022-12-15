e335e90f7ea60fad428ccdab4799879cee541b1a
Ignacio Hardoy <hartoyy@hotmail.com>
2022-12-15 11:36:06 -0300

    new date

--- countdown.js
+++ countdown.js
@@ -1,33 +1,36 @@
-const myDays = document.getElementById("days")
-const myHours = document.getElementById("hours")
-const myMinutes = document.getElementById("minutes")
-const mySeconds = document.getElementById("seconds")
+const myDays = document.getElementById('days')
+const myHours = document.getElementById('hours')
+const myMinutes = document.getElementById('minutes')
+const mySeconds = document.getElementById('seconds')
 
 function myFunction(x) {
-  x.classList.toggle("change")
-  const mobileMenu = document.getElementById("mobile-menu")
-  mobileMenu.classList.toggle("hide")
+  x.classList.toggle('change')
+  const mobileMenu = document.getElementById('mobile-menu')
+  mobileMenu.classList.toggle('hide')
 
-  const body = document.getElementById("body")
-  body.classList.toggle("no-overflow")
+  const body = document.getElementById('body')
+  body.classList.toggle('no-overflow')
 }
 
 function fixingFooter() {
-  const footer = document.getElementById("footer")
-  footer.classList.toggle("add-margin-left")
+  const footer = document.getElementById('footer')
+  footer.classList.toggle('add-margin-left')
 
-  const lopito = document.getElementById("lopito")
-  lopito.classList.toggle("hide")
+  const lopito = document.getElementById('lopito')
+  lopito.classList.toggle('hide')
 }
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
-const countToDate = new Date("October 6, 2022 14:00:00")
+const countToDate = new Date('December 18, 2022 12:00:00')
+//console.log(countToDate)
 
 let previousTimeBetweenDates
 setInterval(() => {
   const currentDate = new Date()
+  //console.log(currentDate);
   const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000)
+  //console.log(timeBetweenDates);
   flipAllCards(timeBetweenDates)
   previousTimeBetweenDates = timeBetweenDates
 }, 1000)
@@ -43,23 +46,41 @@ function flipAllCards(time) {
   myMinutes.innerHTML = mins
   mySeconds.innerHTML = secs
 
-  flip(document.querySelector("[data-days]"), days)
-  flip(document.querySelector("[data-hours]"), hours)
-  flip(document.querySelector("[data-minutes]"), mins)
-  flip(document.querySelector("[data-seconds]"), secs)
+  flip(document.querySelector('[data-days]'), days + 1)
+  flip(document.querySelector('[data-hours]'), hours + 1)
+  flip(document.querySelector('[data-minutes]'), mins + 1)
+  flip(document.querySelector('[data-seconds]'), secs + 1)
 }
 
 function flip(flipCard, newNumber) {
-  const thatNumber = flipCard.querySelector(".number")
+  const thatNumber = flipCard.querySelector('.number')
   const startNumber = parseInt(thatNumber.textContent)
+  if (newNumber === startNumber) return
 
-  thatNumber.classList.add("animated")
+  thatNumber.classList.add('animacion')
 
-  thatNumber.addEventListener("animationstart", (e) => {
-    thatNumber.textContent = newNumber
-  })
+  const animated = document.querySelector('.animacion')
 
-  thatNumber.addEventListener("animationend", (e) => {
-    thatNumber.classList.remove("animated")
-  })
+  //  animated.addEventListener("animationstart", (e) => {
+  //    thatNumber.textContent = newNumber
+  //    console.log("arranco animacion");
+  //  })
+
+  //  animated.addEventListener("animationend", (e) => {
+  //    console.log("termino animacion");
+  //  })
 }
+
+// create a new instance of 'MutationObserver' named 'observer',
+// passing it a callback function
+observer = new MutationObserver(function (mutationsList, observer) {
+  console.log('cambio minuto')
+})
+
+// call 'observe' on that MutationObserver instance,
+// passing it the element to observe, and the options object
+observer.observe(myMinutes, {
+  characterData: false,
+  childList: true,
+  attributes: false,
+})
